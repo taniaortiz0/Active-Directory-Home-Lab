635ac8b025ff3664decdfbbad1e861c95270328e
Tania Ortiz <tiortiz@proton.me>
2025-05-30 12:21:14 -0500

    Add files via upload
    *  I downloaded a custom PowerShell script and a text file (names of users) to create (+1000) users for the AD Lab. I edited the PowerShell script to implement password security.

---
+++ 1_CREATE_USERS.ps1
@@ -0,0 +1,24 @@
+﻿$PASSWORD_FOR_USERS = "ChangeMe34!."
+$FIRST_LAST_NAMES = Get-Content .\names.txt
+
+# -------------------------------------------- #
+
+$password = ConvertTo-SecureString $PASSWORD_FOR_USERS -AsPlainText -Force
+New-ADOrganizationalUnit -Name _USERS -ProtectedFromAccidentalDeletion $false 
+
+foreach ($n in $FIRST_LAST_NAMES) {
+    $first = $n.Split(" ")[0].ToLower()
+    $last = $n.Split(" ")[1].ToLower()
+    $username = "$(first.Substring(0,1))$($last)".ToLower()
+    Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Green
+
+    New-AdUser -AccountPassword $password `
+               -GivenName $first `
+               -Surname $last `
+               -DisplayName $username `
+               -Name $username `
+               -EmployeeID $username `
+               -PasswordNeverExpires $false `
+               -Path "ou=_USERS.$(([ADSI]`"").distinguishedName)" `
+               -Enabled $true
+}
